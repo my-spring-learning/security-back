c797aeebfc5ae73bdaa2ab8bd81d20a9af2a22d7
lukasbs <lukaszbojes@gmail.com>
2018-10-23 22:31:51 +0200

    Initial commit

---
+++ src/main/java/com/example/securityback/SecuritybackApplication.java
@@ -0,0 +1,12 @@
+package com.example.securityback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritybackApplication.class, args);
+	}
+}
